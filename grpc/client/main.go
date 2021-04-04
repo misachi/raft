464ea5a30dc16426c9c1f6e7723906a23950317f
misachi <misachibrian@gmail.com>
2021-04-04 15:55:09 +0300

    Move getbuffersize function to util file

--- grpc/client/main.go
+++ grpc/client/main.go
@@ -3,14 +3,13 @@ package main
 import (
 	"log"
 	"os"
-	"unsafe"
 
 	"github.com/misachi/raft"
 )
 
 func main() {
 	/* Client test code */
-	buf := make([]byte, int(unsafe.Sizeof(raft.Node{})))
+	buf := make([]byte, raft.GetBufferSize())
 	node := raft.ReadNodeFile(buf, os.O_RDONLY)
 	if node == nil {
 		log.Fatal("The server is not running. Ensure the server is started.")
--- grpc/server/main.go
+++ grpc/server/main.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"net"
 	"os"
-	"unsafe"
 
 	"github.com/misachi/raft"
 	pb "github.com/misachi/raft/protos/requestvote"
@@ -17,19 +16,8 @@ type RequestVoteServer struct {
 	pb.UnimplementedRequestVoteServer
 }
 
-func getBufferSize() int {
-	var bufSize = int(unsafe.Sizeof(raft.Node{}))
-	fInfo, err := os.Stat(raft.NodeDetail)
-	if err != nil {
-		fmt.Printf("File Stat error: %v", err)
-	} else {
-		bufSize = int(fInfo.Size())
-	}
-	return bufSize
-}
-
 func (r *RequestVoteServer) GetVote(ctx context.Context, detail *pb.RequestVoteDetail) (*pb.RequestVoteResponse, error) {
-	buf := make([]byte, getBufferSize())
+	buf := make([]byte, raft.GetBufferSize())
 	node := raft.ReadNodeFile(buf, os.O_RDONLY)
 	term, voted := node.VoteForClient(detail.CandidateId, detail.Term, detail.LastLogIndex, detail.LastLogTerm)
 	if err := node.PersistToDisk(0644, os.O_CREATE|os.O_WRONLY); err != nil {
@@ -47,7 +35,7 @@ func main() {
 		log.Fatalf("Error starting up server: %v", err)
 	}
 
-	var bufSize = getBufferSize()
+	var bufSize = raft.GetBufferSize()
 
 	buf := make([]byte, bufSize)
 	node := raft.ReadNodeFile(buf, os.O_CREATE|os.O_RDONLY)
