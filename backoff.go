76efafca8ba56594dc020c76cc36eee0db064d68
misachi <misachibrian@gmail.com>
2021-04-04 22:35:21 +0300

    Add backoff implementation for retrials

---
+++ backoff.go
@@ -0,0 +1,51 @@
+/* The implementation is borrowed from https://github.com/lestrrat-go/backoff */
+
+package raft
+
+import "time"
+
+type Exponential struct {
+	minInterval float64
+	maxInterval float64
+	current     float64
+	multiplier  float64
+}
+
+var defaultMultiplier = 1.5
+
+func NewExponentialInterval(optMin, optMax, mult float64) *Exponential {
+	if optMin > optMax {
+		optMin = optMax
+	}
+
+	if mult <= 1 {
+		mult = defaultMultiplier
+	}
+
+	return &Exponential{
+		minInterval: optMin,
+		maxInterval: optMax,
+		multiplier:  mult,
+	}
+}
+
+func (e *Exponential) Next() time.Duration {
+	var next float64
+	if e.current == 0 {
+		next = e.minInterval
+	} else {
+		next = e.current * e.multiplier
+	}
+
+	if next > e.maxInterval {
+		next = e.maxInterval
+	}
+
+	if next < e.minInterval {
+		next = e.minInterval
+	}
+
+	e.current = next
+	return time.Duration(next)
+
+}
